d654a6ab25ac20c0dd4431978cbda63367dc03e9
Thomas Wilkinson <twilkinson3421@outlook.com>
2025-07-30 13:46:28 +0100

    fix(main): resolve main result before exit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export function Main<Return>(method: () => Return): typeof method;
 export function Main<Return>(method: (args: string[]) => Return): typeof method;
 export function Main<Return>(method: (args: string[]) => Return): typeof method {
     const result = method(Deno.args);
-    if (typeof result === "number") Deno.exit(result);
+
+    Promise.resolve(result).then(result => {
+        if (typeof result === "number") Deno.exit(result);
+    });
+
     return method;
 }
